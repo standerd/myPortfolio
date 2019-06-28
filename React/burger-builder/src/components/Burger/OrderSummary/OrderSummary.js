ad92d74bde7c8baaf753b3e9c3d09f7bc3eacd89
Dewald <Dewald@Dewalds-MacBook-Pro.local>
2019-06-28 13:10:37 +0200

    Ajax test intial setup

---
+++ React/burger-builder/src/components/Burger/OrderSummary/OrderSummary.js
@@ -0,0 +1,40 @@
+import React, { Component } from "react";
+import Aux from "../../../hoc/Aux";
+import Button from "../../UI/Button/Button";
+
+class OrderSummary extends Component {
+  componentWillUpdate(){
+    console.log('[orderSummary]')
+  }
+
+  render() {
+    const ingredientSummary = Object.keys(this.props.ingredients).map(igKey => {
+      return (
+        <li key={igKey}>
+          <span style={{ textTransform: "capitalize" }}>{igKey}</span>:
+          {this.props.ingredients[igKey]}
+        </li>
+      );
+    });
+
+    return (
+      <Aux>
+        <h3>Your Order</h3>
+        <p>A delicious burger with the following ingredients:</p>
+        <ul>{ingredientSummary}</ul>
+        <p>
+          <strong>Total Price: {this.props.price.toFixed(2)}</strong>
+        </p>
+        <p>Continue to checkout</p>
+        <Button btnType="Danger" clicked={this.props.purchaseCancelled}>
+          CANCEL
+        </Button>
+        <Button btnType="Success" clicked={this.props.purchaseContinue}>
+          CONTINUE
+        </Button>
+      </Aux>
+    );
+  }
+}
+
+export default OrderSummary;
